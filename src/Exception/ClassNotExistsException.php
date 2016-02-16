5b287b7f5575a7783058ecc34b12aaa4d19e9620
Krystian Kuczek <krystian@hexmedia.pl>
2016-02-17 00:39:38 +0100

    Writter fixes.

---
+++ src/Exception/ClassNotExistsException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author    Krystian Kuczek <krystian@hexmedia.pl>
+ * @copyright 2013-2016 Hexmedia.pl
+ * @license   @see LICENSE
+ */
+
+namespace Hexmedia\Crontab\Exception;
+
+
+class ClassNotExistsException
+{
+
+}
